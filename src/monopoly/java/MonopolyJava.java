2e8943a05e967cfa42412b5c77f91d9a2f4bec58
Ryan Supawarapong <ryan3311@hotmail.com>
2023-09-30 13:36:44 +0200

    init Java project

---
+++ src/monopoly/java/MonopolyJava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package monopoly.java;
+
+/**
+ *
+ * @author work
+ */
+public class MonopolyJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
