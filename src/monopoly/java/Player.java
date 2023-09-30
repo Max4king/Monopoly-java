3510554eecd2b0c6107521edd96c75d668ab5780
Ryan Supawarapong <ryan3311@hotmail.com>
2023-09-30 14:15:47 +0200

    create player class

---
+++ src/monopoly/java/Player.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package monopoly.java;
+
+/**
+ *
+ * @author work
+ */
+public class Player {
+    private int money;
+    private String name;
+    
+    public Player(String name) {
+        this.name = name;
+        this.money = 10_000;
+    }
+    
+}
